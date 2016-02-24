502169cb67417085da0ca0bae25c6ee28cecfcf5
Jeremiah Almazar <jemalmazar@gmail.com>
2016-02-24 11:26:03 -0800

    Setup GET and POST requests in CheckInCTRL

--- src/app/check-in/CheckInCtrlL.js
+++ src/app/check-in/CheckInCtrlL.js
@@ -6,8 +6,37 @@
     .controller('CheckInCtrl', CheckInCtrl);
 
   /** @ngInject */
-  function CheckInCtrl($scope) {
-      this.online = true;
+  function CheckInCtrl($scope, $http) {
+    var JOBS_GET_URL = 'https://red-wdp-api.herokuapp.com/api/mars/jobs';
+    var COLONIST_POST_URL = 'https://red-wdp-api.herokuapp.com/api/mars/colonists';
+    // placeholder object for POST request to /colonists
+    $scope.colonist = {};
+    // fetch jobs data from API
+    $http({
+      method: 'GET',
+      url: JOBS_GET_URL
+    }).then(function(response){
+        $scope.joblist = response.data.jobs;
+    }, function(error){
+        //
+    });
+
+    $scope.login = function(e){
+      e.preventDefault();
+
+      $http({
+        method: 'POST',
+        url: COLONIST_POST_URL,
+        data: {
+          'colonist': $scope.colonist
+        }
+      }).then(function(response){
+        console.log(response);
+      }, function(error){
+        console.log(error);
+      });
+    };
+
   }
 
 })();
